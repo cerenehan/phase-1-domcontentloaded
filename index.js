f99c0590811bf5cc87b99efd437a688ad646fd8f
Colleenrenehan <cerenehan5@gmail.com>
2023-10-05 17:09:48 -0500

    coplete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+
+  
+document.addEventListener("DOMContentLoaded", function updateTextContent(text) {
+   let p = document.querySelector('p')
+   p.innerHTML = 'This is really cool!'
+   console.log("The DOM has loaded");
+    updateTextContent('This is Really cool!')
+  });
+
